9039930a6b2060382e8060e56270435ef1f02b26
Christian Nastasi <christian.nastasi@gmail.com>
2024-12-11 17:18:18 +0100

    Solved also part2

--- src/Day11/Exercise2.php
+++ src/Day11/Exercise2.php
@@ -4,40 +4,50 @@ namespace DevDojo\Day11;
 
 class Exercise2
 {
+    private static array $cache = [0 => 1, 1 => 2024, 2024 => [20, 24]];
+
     public static function execute(string $input): int
     {
-        $numbers = explode(' ', $input);
+        $numbers = array_map(intval(...), explode(' ', $input));
 
-        $sum = 0;
-
-        foreach ($numbers as $number) {
-            $sum += static::blink($number, 0);
-        }
-
-        return $sum;
+        return static::walk($numbers, 0);
     }
 
-    public static function blink(int $number, int $count): int
+    public static function walk(array $stones): int
     {
-//        $memory = (int)(memory_get_usage() / 1024);/**/
+        $reduce = function ($carry, $stone) {
+            return $carry + static::cachedBlink($stone, 0);
+        };
 
-        if ($count === 75) {
+        return array_reduce($stones, $reduce, 0);
+    }
+
+    public static function cachedBlink(int $number, int $count): int
+    {
+        return static::$cache["{$number}_{$count}"] ??= static::blink($number, $count);
+    }
+
+    public static function blink(int $stone, int $step): int
+    {
+        if ($step === 75) {
             return 1;
         }
 
-        if ($number === 0) {
-            return static::blink(1, $count + 1);
-        } else if (static::hasEvenDigits($number)) {
-            $numberAsString = (string)$number;
+        if ($stone === 0) {
+            return static::cachedBlink(1, $step + 1);
+        }
+
+        if (static::hasEvenDigits($stone)) {
+            $numberAsString = (string)$stone;
             $length = strlen($numberAsString);
 
             $leftPart = substr($numberAsString, 0, $length / 2);
             $rightPart = substr($numberAsString, $length / 2, $length / 2);
 
-            return static::blink((int)$leftPart, $count + 1) + static::blink((int)$rightPart, $count + 1);
+            return static::cachedBlink((int)$leftPart, $step + 1) + static::cachedBlink((int)$rightPart, $step + 1);
         }
 
-        return static::blink($number * 2024, $count + 1);
+        return static::cachedBlink($stone * 2024, $step + 1);
     }
 
     public static function hasEvenDigits(int $number): bool
